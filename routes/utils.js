66e84f0a34ff9b66d25c1c3499b44c163208a395
stefanoauciello <auciellostefano1@gmail.com>
2019-02-12 16:27:16 +0100

    add mongoose endpoint in /utils

---
+++ routes/utils.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-use-before-define */
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+const Authentication = require('./../authentication');
+
+router.get('/mongoose', Authentication, HandleDBJson);
+
+async function HandleDBJson(req, res) {
+  const models = mongoose.modelNames();
+
+  const allData = await Promise.all(models.map(async (model) => {
+    // eslint-disable-next-line import/no-dynamic-require,global-require
+    const fileModel = require(`./../modelsNoSQL/${model}`);
+    return fileModel.find();
+  }));
+
+  res.json(allData);
+}
+
+module.exports = router;
