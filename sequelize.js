39fbe19fd24a0a0dbcc83df87c1aaa93ca6e6184
stefanoauciello <auciellostefano1@gmail.com>
2019-02-13 13:06:17 +0100

    transform sequelize in class

--- sequelize.js
+++ sequelize.js
@@ -3,26 +3,31 @@ const Sequelize = require('sequelize');
 // initial connection database
 console.log('Connecting to database...');
 
-const sequelize = new Sequelize('simpleReactNodeJS', 'root', 'rootroot', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
+class MySequelize extends Sequelize {
+  constructor() {
+    super('simpleReactNodeJS', 'root', 'rootroot', {
+      host: 'localhost',
+      dialect: 'mysql',
+      operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    });
+    this.connect();
+  }
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('MySql DB connected!');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+  connect() {
+    return this.authenticate().then(() => {
+      console.log('MySql DB connected!');
+    })
+      .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+      });
+  }
+}
 
-module.exports = sequelize;
+module.exports = new MySequelize();
