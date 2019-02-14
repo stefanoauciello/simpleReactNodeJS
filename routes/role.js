148df5c8afa8450b8e1d0fcf0ac59bc94d621b92
stefanoauciello <auciellostefano1@gmail.com>
2019-02-14 11:03:44 +0100

    fix role endpoint

--- routes/role.js
+++ routes/role.js
@@ -11,22 +11,28 @@ router.delete('/:id', Authentication, HandleDeleteRole);
 
 async function HandleCreateRole(req, res) {
   const { name } = req.body;
-  await Role.create({ name });
-  res.json({ status: 200 });
+  if (!name) {
+    return res.status(400).json({ error: 'name not found' });
+  }
+  const role = await Role.create({ name });
+  res.status(200).json(role);
 }
 
 async function HandleDeleteRole(req, res) {
   const { id } = req.params;
-  const role = await Role.findOne({ where: { id } });
-  if (role) {
-    await role.destroy();
-    res.json({
-      success: true,
-      message: 'Deleted',
-    });
-  } else {
-    res.status(404).json({ error: 'Role not found' });
+  if (!id) {
+    return res.status(400).json({ error: 'id not found' });
   }
+  const role = await Role.findOne({ where: { id } });
+  if (!role) {
+    return res.status(404).json({ error: 'Role not found' });
+  }
+
+  await role.destroy();
+  return res.status(200).json({
+    success: true,
+    message: 'Deleted',
+  });
 }
 
 module.exports = router;
